c5bcde2a95a356512baecdf51a90e983fa05ea96
Ikhor03 <imamkhoiri725@gmail.com>
2023-01-14 15:24:33 +0700

    task 19

---
+++ tugas19.js
@@ -0,0 +1,11 @@
+console.log("Semangat...".repeat(5));
+
+console.log("*".repeat(5));
+console.log("*".repeat(20));
+console.log("*".repeat(30));
+console.log("*".repeat(40));
+console.log("*".repeat(60));
+console.log("*".repeat(40));
+console.log("*".repeat(30));
+console.log("*".repeat(20));
+console.log("*".repeat(5));
